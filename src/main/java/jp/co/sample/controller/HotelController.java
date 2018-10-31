400f1a0a6a99f479c8094187fd4ff439c0faf1cf
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-10-31 17:32:04 +0900

    findByPriceメソッドが使えるようにコントローラを変更

--- src/main/java/jp/co/sample/controller/HotelController.java
+++ src/main/java/jp/co/sample/controller/HotelController.java
@@ -1,7 +1,14 @@
 package jp.co.sample.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.domain.Hotel;
+import jp.co.sample.repository.HotelRepository;
 
 /**
  * 入出力画面へフォワードするコントローラ.
@@ -12,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/hotel")
 public class HotelController {
+	
+	@Autowired
+	private HotelRepository repository;
 	
 	/**
 	 * 入力画面を表示
@@ -24,7 +34,12 @@ public class HotelController {
 	}
 	
 	@RequestMapping("/result")
-	public String result() {
+	public String result(Integer price, Model model) {
+		
+		List<Hotel> hotelList = repository.findByPrice(price);
+		
+		model.addAttribute(hotelList);
+		
 		return "hotelResult";
 	}
 	
