9ee20e4b42ddf3dd55ae886543a7c5173f75f447
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-11-01 09:46:02 +0900

    入出力画面にフォワードするコントローラ

---
+++ src/main/java/jp/co/sample/controller/ClothesController.java
@@ -0,0 +1,59 @@
+package jp.co.sample.controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.form.ClothesForm;
+import jp.co.sample.repository.ClothesRepository;
+
+/**
+ * 入出力画面にフォワードするコントローラ.
+ * 
+ * @author junpei.oyama
+ *
+ */
+@Controller
+@RequestMapping("/clothes")
+public class ClothesController {
+	
+	/** 利用するリポジトリをDI */
+	@Autowired
+	private ClothesRepository repository;
+	
+	@ModelAttribute
+	public ClothesForm setUpForm() {
+		return new ClothesForm();
+	}
+	
+	@RequestMapping("/index")
+	public String index(Model model) {
+		
+		Map<Integer,String> colorMap = new LinkedHashMap<>();
+		colorMap.put(1, "赤");
+		colorMap.put(2, "青");
+		colorMap.put(3, "黄");
+		colorMap.put(4, "白");
+		
+		model.addAttribute("colorMap", colorMap);
+		
+		return "indexShop";
+	}
+	
+//	@RequestMapping("/search")
+//	public String search(ClothesForm form, RedirectAttributes redirectAttributes) {
+//		
+//		Clothes clothes = new Clothes();
+//		
+//		clothes.setGender(form.getGender());
+//		clothes.setColor(form.getColor());
+//		
+//	}
+	
+}
+
